ffbdd498695176b0e922ff65b83f2b8d391a4ce1
manbcdiachuk13091986 <manbcdiachuk@gmail.com>
2025-09-21 11:41:22 +0200

    Add task 1 - domain

--- src/main/java/app/domain/Doctor.java
+++ src/main/java/app/domain/Doctor.java
@@ -1,4 +1,74 @@
 package app.domain;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
 public class Doctor {
+    private int id;
+    private String name;
+    private boolean active;
+    private List<Pat> pats = new ArrayList<>();
+
+    public Doctor() {
+    }
+
+    public Doctor(String name) {
+        this.name = name;
+    }
+
+    public Doctor(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    public List<Pat> getProducts() {
+        return pats;
+    }
+
+    public void setPats(List<Pat> pats) {
+        this.pats = pats;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Doctor doctor = (Doctor) o;
+        return id == doctor.id && active == doctor.active && Objects.equals(name, doctor.name) && Objects.equals(pats, doctor.pats);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, active, pats);
+    }
+
+
+    @Override
+    public String toString() {
+        return String.format("Доктор: id - %d, имя - %s, активен - %b, лист покупок - %s.",
+                id, name, active, pats);
+    }
 }
--- src/main/java/app/domain/Pat.java
+++ src/main/java/app/domain/Pat.java
@@ -1,4 +1,81 @@
 package app.domain;
 
-public class Pat  {
+import java.util.Objects;
+
+public class Pat {
+    private int id;
+    private String title;
+    private double price;
+    private boolean active;
+
+    public Pat() {
+    }
+
+    public Pat(String title, double price) {
+        this.title = title;
+        this.price = price;
+    }
+
+    public Pat(int id, double price) {
+        this.id = id;
+        this.price = price;
+    }
+
+    public Pat(int id, String title, double price, boolean active) {
+        this.id = id;
+        this.title = title;
+        this.price = price;
+        this.active = active;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Pat pat = (Pat) o;
+        return id == pat.id && Double.compare(price, pat.price) == 0 && active == pat.active && Objects.equals(title, pat.title);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, title, price, active);
+    }
+
+
+    @Override
+    public String toString() {
+        return String.format("т: id Пациент- %d, наименование - %s, цена - %.2f, активен - %b.",
+                id, title, price, active);
+    }
 }
